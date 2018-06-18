3e8ce176342d7dc9202ecdc98549a2e71957216e
May <tusek.nicole@gmail.com>
2018-06-17 18:16:28 -0700

    Need to work on replacement parser.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var app = express();
 var app_name = get(serverConfig, 'Name');
 var app_description = get(serverConfig, 'Config');
 var app_port = get(serverConfig, 'Port');
+var app_frontpage_path = get(serverConfig, 'Frontpage_Path');
 
 var app_upload_endpoint = get(serverConfig, 'Upload_Endpoint');
 var app_download_endpoint = get(serverConfig, 'Download_Endpoint');
@@ -31,8 +32,8 @@ app.post(app_upload_endpoint, (req, resp) => {
 
 /* Serve page. */
 app.get('/', (req, res) => {
-    console.log('REQed page.');
-})
+     
+});
 
 /* Handle downloads. */
 app.get(app_download_endpoint, (req, res) => {
@@ -40,10 +41,45 @@ app.get(app_download_endpoint, (req, res) => {
 });
 
 /**
+ * 
+ * @param {String} str string to find and replace with from templateObj.
+ * @param {*} templateObj 
+ */
+function templateLookup(str, templateObj) {
+
+}
+
+/**
+ * @desc Generate text from a template with replacements. 
+ * @param {String} str string to find things and replace with things.
+ * @param {JSON Obj} templateObj a JSON representative of the {params} to replace.
+ * @returns {String} the generated text from the template.
+ */
+function generateFromTemplate(str, templateObj) {
+    let occurenceIndex = 0;
+
+    while(str.indexOf('{', occurenceIndex) != -1 && str.indexOf('}') != -1) {
+        startCurly = occurenceIndex = str.indexOf('{', occurenceIndex);
+        endCurly = str.indexOf('}', occurenceIndex);
+
+        templateLookup()
+    }
+
+    /** [Example] 
+     * var str = 'that is {adjective}'
+     * var template = {adjective: 'cool'}
+     * generateFromTemplate(str, template);
+     * 
+     * [Output]
+     * that is cool
+     */
+}
+
+/**
  * @desc Returns a parameter from a JSON object.
  * @param {JSON Obj} obj the JSON object to extract the parameter from.
  * @param {String} param the parameter to extract from the JSON object. 
- * @returns {String | JSON Obj} 
+ * @returns {String | JSON Obj} the value of the param from the JSON object.
  */
 function get(obj, param) {
     return obj[param];
@@ -52,7 +88,7 @@ function get(obj, param) {
 /**
  * @desc Turns a string into a JSON object.
  * @param {String} str string to parse.
- * @returns {JSON Obj} 
+ * @returns {JSON Obj}  returns the new JSONified string.
  */
 function parse(str) {
     return JSON.parse(str);
